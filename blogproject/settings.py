326f5366c77fa0bd105e44860c898dca5fdd4dcf
codehermit <lixingying@gmail.com>
2017-11-20 20:45:24 +0800

    change database

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'blog',
+         'USER': 'root',
+         'PASSWORD': 'Lixy@0866',
+         'HOST': '',
+         'PORT': '',
+         'OPTIONS': {
+            'autocommit': True,
+         },
     }
 }
 
